c7e0dcc227e53cdd3bb3f4a741ec803ded9a1eb1
sarthaksinghal@pm.me <sarthaksinghal@pm.me>
2025-09-13 12:39:28 +0530

    changed some things

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,8 @@ import mongoose from "mongoose";
 import cors from "cors";
 import { connectToSocket } from "./controllers/socketManager.js";
 import userRoutes from "./routes/users.routes.js";
-
+import dotenv from "dotenv";
+dotenv.config();
 
 const MONGO_URL = process.env.MONGO_DB_URL;
 const app = express();
